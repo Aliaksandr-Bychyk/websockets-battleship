cee5a361b3f9447afc74f359c9faa4f946ad8382
Aliaksandr <sanya55bychyk@gmail.com>
2023-07-08 13:17:07 +0200

    feat: add deepParser

---
+++ src/utils/deepParser.ts
@@ -0,0 +1,12 @@
+const deepParser = (str: string) => {
+  const data = JSON.parse(str);
+  for (const property in data) {
+    if (typeof data[property] === 'string') {
+      if (data[property].includes('{'))
+        data[property] = deepParser(data[property]);
+    }
+  }
+  return data;
+};
+
+export default deepParser;
